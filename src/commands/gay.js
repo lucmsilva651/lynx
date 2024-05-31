78625caf2401d2ce188cc07f94ba214d0cdd4c79
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-05-31 17:42:59 -0300

    Fixed spacing in gay

--- src/commands/gay.js
+++ src/commands/gay.js
@@ -1,24 +1,24 @@
-module.exports = function(bot, msg) {
-    const chatId = msg.chat.id;
-    const userName = msg.from.first_name;
-    const userId = msg.from.id;
-    let isGay = "";
-  
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    }
-  
-    const randomValue = getRandomInt(2);
-  
-    if (randomValue === 0) {
-      isGay = `${userName} is not gay.`;
-    } else {
-      isGay = `${userName} is a gay.`;
-    }
-  
-    const message = `${isGay}`;
-    
-    bot.sendMessage(chatId, message)
-      .catch(error => console.error('ERROR: Message cannot be sent:', error));
-    console.log(`INFO: /gay executed by ${userName}, ${userId}`)
+module.exports = function (bot, msg) {
+  const chatId = msg.chat.id;
+  const userName = msg.from.first_name;
+  const userId = msg.from.id;
+  let isGay = "";
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
+
+  const randomValue = getRandomInt(2);
+
+  if (randomValue === 0) {
+    isGay = `${userName} is not gay.`;
+  } else {
+    isGay = `${userName} is a gay.`;
+  }
+
+  const message = `${isGay}`;
+
+  bot.sendMessage(chatId, message)
+    .catch(error => console.error('ERROR: Message cannot be sent:', error));
+  console.log(`INFO: /gay executed by ${userName}, ${userId}`)
+}
