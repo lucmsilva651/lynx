44760b8e21038b89b020080df0d0341544c60578
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-06-02 15:32:36 +0000

    Update customize

--- commands/customize.js
+++ commands/customize.js
@@ -1,21 +1,21 @@
 module.exports = function(bot, msg) {
-    const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-    const opts = {
-        reply_to_message_id: msg.message_id,
-        reply_markup: {
-            resize_keyboard: true,
-            one_time_keyboard: true,
-            keyboard: [
-              [{text: 'He/Him'}],
-              [{text: 'She/Her'}],
-              [{text: 'They/Them'}],
-            ],
-        }
-    };
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: {
+      resize_keyboard: true,
+      one_time_keyboard: true,
+      keyboard: [
+        [{text: 'He/Him'}],
+        [{text: 'She/Her'}],
+        [{text: 'They/Them'}],
+      ],
+    }
+  };
 
-    const message = "Select your pronouns:";
+  const message = "Select your pronouns:";
 
-    bot.sendMessage(chatId, message, opts,{ parse_mode: 'Markdown' })
-    .catch(error => console.error('WARN: Message cannot be sent: ', error));
+  bot.sendMessage(chatId, message, opts,{ parse_mode: 'Markdown' })
+  .catch(error => console.error('WARN: Message cannot be sent: ', error));
 }
