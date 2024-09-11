177a3c55408ae7c249a7b3a6416b39183e76cc39
lucmsilva651 <lucmsilva651@gmail.com>
2024-09-11 18:45:25 -0300

    Put help in another file to avoid confusion

---
+++ commands/help.js
@@ -0,0 +1,67 @@
+const { getStrings } = require('../plugins/checklang.js');
+const { isOnSpamWatch } = require('../plugins/lib-spamwatch/spamwatch.js');
+const spamwatchMiddleware = require('../plugins/lib-spamwatch/Middleware.js')(isOnSpamWatch);
+
+async function sendHelpMessage(ctx, isEditing) {
+  const Strings = getStrings(ctx.from.language_code);
+  const options = {
+    parse_mode: 'Markdown',
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: Strings.mainCommands, callback_data: '1' }, { text: Strings.usefulCommands, callback_data: '2' }],
+        [{ text: Strings.interactiveEmojis, callback_data: '3' }, { text: Strings.funnyCommands, callback_data: '4' }]
+      ]
+    }
+  };
+  const helpText = Strings.lynxHelp;
+  if (isEditing) {
+    await ctx.editMessageText(helpText, options);
+  } else {
+    await ctx.reply(helpText, options);
+  }
+}
+
+module.exports = (bot) => {
+  bot.help(spamwatchMiddleware, async (ctx) => {
+    await sendHelpMessage(ctx);
+  });
+
+  bot.on('callback_query', async (ctx) => {
+    const callbackData = ctx.callbackQuery.data;
+    const Strings = getStrings(ctx.from.language_code);
+    const options = {
+      parse_mode: 'Markdown',
+      reply_markup: JSON.stringify({
+        inline_keyboard: [
+          [{ text: Strings.goBack, callback_data: '5' }],
+        ]
+      })
+    };
+
+    switch (callbackData) {
+      case '1':
+        await ctx.answerCbQuery();
+        await ctx.editMessageText(Strings.mainCommandsDesc, options);
+        break;
+      case '2':
+        await ctx.answerCbQuery();
+        await ctx.editMessageText(Strings.usefulCommandsDesc, options);
+        break;
+      case '3':
+        await ctx.answerCbQuery();
+        await ctx.editMessageText(Strings.interactiveEmojisDesc, options);
+        break;
+      case '4':
+        await ctx.answerCbQuery();
+        await ctx.editMessageText(Strings.funnyCommandsDesc, options);
+        break;
+      case '5':
+        await ctx.answerCbQuery();
+        await sendHelpMessage(ctx, true);
+        break;
+      default:
+        await ctx.answerCbQuery(Strings.invalidOption);
+        break;
+    }
+  });
+}
--- commands/main.js
+++ commands/main.js
@@ -3,25 +3,6 @@ const { getStrings } = require('../plugins/checklang.js');
 const { isOnSpamWatch } = require('../plugins/lib-spamwatch/spamwatch.js');
 const spamwatchMiddleware = require('../plugins/lib-spamwatch/Middleware.js')(isOnSpamWatch);
 
-async function sendHelpMessage(ctx, isEditing) {
-  const Strings = getStrings(ctx.from.language_code);
-  const options = {
-    parse_mode: 'Markdown',
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: Strings.mainCommands, callback_data: '1' }, { text: Strings.usefulCommands, callback_data: '2' }],
-        [{ text: Strings.interactiveEmojis, callback_data: '3' }, { text: Strings.funnyCommands, callback_data: '4' }]
-      ]
-    }
-  };
-  const helpText = Strings.lynxHelp;
-  if (isEditing) {
-    await ctx.editMessageText(helpText, options);
-  } else {
-    await ctx.reply(helpText, options);
-  }
-}
-
 module.exports = (bot) => {
   bot.start(spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
@@ -32,49 +13,6 @@ module.exports = (bot) => {
       reply_to_message_id: ctx.message.message_id
     }
     );
-  });
-
-  bot.help(spamwatchMiddleware, async (ctx) => {
-    await sendHelpMessage(ctx);
-  });
-
-  bot.on('callback_query', async (ctx) => {
-    const callbackData = ctx.callbackQuery.data;
-    const Strings = getStrings(ctx.from.language_code);
-    const options = {
-      parse_mode: 'Markdown',
-      reply_markup: JSON.stringify({
-        inline_keyboard: [
-          [{ text: Strings.goBack, callback_data: '5' }],
-        ]
-      })
-    };
-
-    switch (callbackData) {
-      case '1':
-        await ctx.answerCbQuery();
-        await ctx.editMessageText(Strings.mainCommandsDesc, options);
-        break;
-      case '2':
-        await ctx.answerCbQuery();
-        await ctx.editMessageText(Strings.usefulCommandsDesc, options);
-        break;
-      case '3':
-        await ctx.answerCbQuery();
-        await ctx.editMessageText(Strings.interactiveEmojisDesc, options);
-        break;
-      case '4':
-        await ctx.answerCbQuery();
-        await ctx.editMessageText(Strings.funnyCommandsDesc, options);
-        break;
-      case '5':
-        await ctx.answerCbQuery();
-        await sendHelpMessage(ctx, true);
-        break;
-      default:
-        await ctx.answerCbQuery(Strings.invalidOption);
-        break;
-    }
   });
 
   bot.command('privacy', spamwatchMiddleware, async (ctx) => {
