c49664867f552d4a1cceb7e399446ef89f38f36c
A Bunch of Computer Nerds <abocn@protonmail.com>
2024-12-08 21:03:33 -0300

    Removed Luna photo from bot

--- src/commands/main.js
+++ src/commands/main.js
@@ -6,9 +6,7 @@ const spamwatchMiddleware = require('../plugins/lib-spamwatch/Middleware.js')(is
 module.exports = (bot) => {
   bot.start(spamwatchMiddleware, async (ctx) => {
     const Strings = getStrings(ctx.from.language_code);
-    ctx.replyWithPhoto(
-      resources.lunaCat, {
-      caption: Strings.lynxWelcome,
+    ctx.reply(Strings.lynxWelcome, {
       parse_mode: 'Markdown',
       reply_to_message_id: ctx.message.message_id
     });
